b41b2a440ec5f8a91267d1ca164db937874395f8
ELTEGANI MOHAMED <elteganimohamed@ELTEGANIs-MacBook-Air.local>
2022-04-07 01:34:34 +0400

    add recommeded food detailes

---
+++ lib/pages/food/recommended_food_detail.dart
@@ -0,0 +1,134 @@
+import 'package:e_commerace/utils/colors.dart';
+import 'package:e_commerace/utils/dimensions.dart';
+import 'package:e_commerace/widgets/app_icon.dart';
+import 'package:e_commerace/widgets/big_text.dart';
+import 'package:e_commerace/widgets/expandable_text_widget.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RecommendedFoodDetail extends StatelessWidget {
+  const RecommendedFoodDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: CustomScrollView(
+           slivers: [
+             SliverAppBar(
+               toolbarHeight:70,
+               title:Row(
+                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                 children: [
+                   AppIcon(iconData: Icons.clear),
+                   AppIcon(iconData: Icons.shopping_cart_outlined)
+                 ],
+               ),
+               bottom: PreferredSize(
+                 preferredSize: Size.fromHeight(20),
+                 child:Container(
+                   child:Center(child:BigText(size:Dimensions.font26,text:"Chinese Side")),
+                   width:double.maxFinite,
+                   padding:EdgeInsets.only(top:5,bottom:10),
+                   decoration:BoxDecoration(
+                       color:Colors.white,
+                       borderRadius: BorderRadius.only(
+                       topLeft:Radius.circular(Dimensions.radius20),
+                       topRight:Radius.circular(Dimensions.radius20)
+                     )
+                   ),
+                 ),
+               ),
+               pinned: true,
+               backgroundColor: AppColors.yellowColor,
+               expandedHeight: 300,
+               flexibleSpace: FlexibleSpaceBar(
+                  background: Image.asset(
+                    "assets/image/food0.png",
+                     width:double.maxFinite,
+                     fit: BoxFit.cover,
+                  ),
+               ),
+             ),
+             SliverToBoxAdapter(
+               child: Column(
+                 children: [
+                   Container(
+                     child: ExpandableTextWidget(text:"remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop.remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktopremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop"),
+                     margin: EdgeInsets.only(left:Dimensions.width20,right:Dimensions.width20),
+                   )
+                 ],
+               )
+             )
+           ],
+        ),
+        bottomNavigationBar:Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              padding:EdgeInsets.only(
+                left: Dimensions.width20*2.5,
+                right: Dimensions.width20*2.5,
+                top: Dimensions.height10,
+                bottom:Dimensions.height10
+              ),
+              child: Row(
+                mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                children: [
+                  AppIcon(
+                      iconSize:Dimensions.iconSize24,
+                      iconColor:Colors.white,backColor:AppColors.mainColor,iconData:Icons.remove),
+                   BigText(text:" \$12.88 "+ " X " + " 0 ",
+                     color:AppColors.mainBlackColor,size:Dimensions.font26),
+                  AppIcon(
+                      iconSize:Dimensions.iconSize24,
+                      iconColor:Colors.white,backColor:AppColors.mainColor,iconData:Icons.add),
+                ],
+              ),
+            ),
+            Container(
+              height: Dimensions.bottomHeightBar,
+              padding: EdgeInsets.only(
+                  top:Dimensions.height30,
+                  bottom: Dimensions.height30,
+                  left: Dimensions.width20,
+                  right: Dimensions.width20
+              ),
+              decoration: BoxDecoration(
+                  color: AppColors.buttonBackgroundColor,
+                  borderRadius: BorderRadius.only(
+                      topLeft:Radius.circular(Dimensions.radius20*2),
+                      topRight: Radius.circular(Dimensions.radius20*2)
+                  )
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(top:Dimensions.height20,bottom: Dimensions.height20,left: Dimensions.width20,right: Dimensions.width20),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(Dimensions.radius20),
+                        color: Colors.white
+                    ),
+                    child: Icon(
+                      Icons.favorite,
+                      color:AppColors.mainColor,
+                      size: 45,
+                    ),
+                  ),
+                  Container(
+                    child:BigText(text:"\$10 | Add to Cart",color: Colors.white,),
+                    padding: EdgeInsets.only(top:Dimensions.height20,bottom: Dimensions.height20,left: Dimensions.width20,right: Dimensions.width20),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(Dimensions.radius20),
+                        color: AppColors.mainColor
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+    );
+  }
+}

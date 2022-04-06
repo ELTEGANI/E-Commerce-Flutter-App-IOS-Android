27f310fe7cf4b8392f7d066c48514c19d45f8280
ELTEGANI MOHAMED <elteganimohamed@ELTEGANIs-MacBook-Air.local>
2022-04-07 01:37:33 +0400

    add recommeded food detailes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:e_commerace/pages/food/popular_food_detail.dart';
-import 'package:e_commerace/pages/home/main_food_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 

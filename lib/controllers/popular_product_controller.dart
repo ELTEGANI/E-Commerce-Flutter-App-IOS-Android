9b09345478d8938b71a360601753c18142e01341
ELTEGANI MOHAMED <elteganimohamed@ELTEGANIs-MacBook-Air.local>
2022-04-12 01:11:38 +0400

    add quantity of product

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -1,5 +1,7 @@
 import 'package:e_commerace/data/repository/popular_product_repo.dart';
 import 'package:e_commerace/models/products_model.dart';
+import 'package:e_commerace/utils/colors.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class PopularProductController extends GetxController{
@@ -9,6 +11,8 @@ class PopularProductController extends GetxController{
   List<dynamic> get popularProductList => _popularProductList;
   bool  _isLoaded = false;
   bool get isLoaded=>_isLoaded;
+  int _quantity = 0;
+  int get quantity=>_quantity;
 
   Future<void> getPopularProductList() async{
      Response response = await popularProductRepo.getPopularProductList();
@@ -21,4 +25,34 @@ class PopularProductController extends GetxController{
        print("response");
      }
   }
+
+  void setQuantity(bool isIncrement){
+    if(isIncrement){
+      _quantity = checkQuantity(_quantity+1);
+    }else{
+      _quantity = checkQuantity(_quantity-1);
+    }
+    update();
+  }
+  int checkQuantity(int quantity){
+    if(quantity<0){
+      Get.snackbar("Item count","You can't reduce more",backgroundColor:AppColors.mainColor,
+      colorText:Colors.white
+      );
+       return 0;
+    }else if (quantity>20){
+      Get.snackbar("Item count","You can't add more",backgroundColor:AppColors.mainColor,
+          colorText:Colors.white
+      );
+       return 20;
+    }else{
+      return quantity;
+    }
+  }
+
+  void initProduct(){
+    _quantity = 0;
+  }
+
+
 }

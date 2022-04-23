54a4abc7d3864c20dfdaeef2a64fd2b8bc585c0b
ELTEGANI MOHAMED <elteganimohamed@ELTEGANIs-MacBook-Air.local>
2022-04-24 01:54:08 +0400

    add cart history

--- lib/pages/cart/cart_history.dart
+++ lib/pages/cart/cart_history.dart
@@ -1,8 +1,10 @@
 import 'package:e_commerace/controllers/cart_controller.dart';
+import 'package:e_commerace/utils/app_constants.dart';
 import 'package:e_commerace/utils/colors.dart';
 import 'package:e_commerace/utils/dimensions.dart';
 import 'package:e_commerace/widgets/app_icon.dart';
 import 'package:e_commerace/widgets/big_text.dart';
+import 'package:e_commerace/widgets/small_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -29,10 +31,10 @@ class CartHistory extends StatelessWidget {
 
     List<int> orderTimes = cartOrderTimeToList();
 
-    print(orderTimes);
+    var listCounter = 0;
+
     return Scaffold(
       body: Column(
-
         children: [
           Container(
             height:100,
@@ -54,14 +56,63 @@ class CartHistory extends StatelessWidget {
                 left:Dimensions.height20,
                 right:Dimensions.height20
               ),
-              child:ListView(
-                children: [
-                  Text("hi there"),
-                  Text("hi there"),
-                  Text("hi there"),
-                  Text("hi there")
-                ],
-              ),
+              child:MediaQuery.removePadding(
+                  removeTop:true,
+                  context: context,
+                  child:ListView(
+                  children: [
+                    for(int i = 0;i < cartItemsPerOrder.length;i++)
+                      Container(
+                        height:120,
+                        margin:EdgeInsets.only(bottom:Dimensions.height20),
+                        child:Column(
+                          crossAxisAlignment:CrossAxisAlignment.start,
+                          children: [
+                            BigText(text: "05/02/2021"),
+                            SizedBox(height:Dimensions.height10,),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Wrap(
+                                  direction:Axis.horizontal,
+                                  children: List.generate(orderTimes[i], (index){
+                                    if(listCounter<getCartHistory.length){
+                                      listCounter++;
+                                    }
+                                    return index<=2?
+                                    Container(
+                                      height:  80,
+                                      width: 80,
+                                      margin:EdgeInsets.only(right:Dimensions.width10/2),
+                                      decoration: BoxDecoration(
+                                          borderRadius:BorderRadius.circular(Dimensions.radius15/2),
+                                          image: DecorationImage(
+                                              fit: BoxFit.cover,
+                                              image: NetworkImage(
+                                                  AppConstants.BASE_URL+AppConstants.UPLOAD_URL+getCartHistory[listCounter-1].img!
+                                              )
+                                          )
+                                      ),
+                                    ):Container();
+                                  }),
+                                ),
+                                Container(
+                                  height:80,
+                                  child:Column(
+                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      SmallText(text: "Total"),
+                                      BigText(text:orderTimes[i].toString(),color:AppColors.titleColor,)
+                                    ],
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                  ]
+              )),
             ),
           )
         ],
